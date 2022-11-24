d8ac4538f45d516422bd0782b46a84e28a40b8c1
lorenzoluzzi <117952492+lorenzoluzzi@users.noreply.github.com>
2022-11-24 21:32:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int n, a=0, b=1, c=0;
+   cout << "inserisci un numero intero" << endl;
+   cin >> n;
+   if (n >=2) {
+      cout << b << endl;
+      for (int 1 = 0; 1<n; 1++) {
+         c = a + b;
+         a = b;
+         b = c;
+         cout << c << endl;
+      }
+   }
+   else {
+      cout << "errore" << endl;
    return 0;
 }
